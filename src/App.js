09e6d340792c15a4b3623828202eaf2bc57a73b1
Ripon Hossain Shuvo <rhshuvo44@gmail.com>
2022-10-29 14:46:28 +0600

    Project ready

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./Component/Home/Home";
+import NotFound from "./Component/Shared/NotFound";
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#FF2D37",
+          secondary: "#19D3AE",
+          accent: "#3A4256",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+      // "dark",
+      // "cupcake",
+    ],
+  },
+  plugins: [require("daisyui")],
+}
