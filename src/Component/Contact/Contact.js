3029e933af768b54d4c17619b41581120d84195f
Ripon Hossain Shuvo <rhshuvo44@gmail.com>
2022-11-26 16:27:52 +0600

    contact section

--- src/Component/Contact/Contact.js
+++ src/Component/Contact/Contact.js
@@ -1,19 +1,47 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 import "./Contact.css";
 
 const Contact = () => {
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data) => console.log(data);
   return (
-    <div className="contact bg-accent p-20 flex">
+    <div className="contact bg-accent p-20 md:flex ">
       <div className="contact-info md:w-1/2">
-        <h2 className="text-[34px] text-[#2D2D2D] px-20">
+        <h2 className="text-4xl text-[#2D2D2D] px-0 md:px-10">
           Let us handle your project, professionally.
         </h2>
-        <p className="px-20 text-[14px] mt-10">
+        <p className="px-0 md:px-10  text-[14px] my-10">
           With well written codes, we build amazing apps for all platforms,
           mobile and web apps in general.
         </p>
       </div>
-      <div className="contact-form md:w-1/2">fffffffff</div>
+      <div className="contact-form md:w-1/2 px-0 md:px-10">
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <input
+            type="email"
+            placeholder="Your email address"
+            className="input w-full"
+            {...register("email", { required: true })}
+          />
+          <input
+            type="text"
+            placeholder="Your name / company’s name"
+            className="input w-full my-5"
+            {...register("name", { required: true })}
+          />
+          <textarea
+            className="textarea w-full h-[277px]"
+            placeholder="Your message"
+            {...register("message", { required: true })}
+          ></textarea>
+          <input
+            type="submit"
+            value="Send"
+            className="dark-btn mb-20 md:mb-0"
+          />
+        </form>
+      </div>
     </div>
   );
 };
